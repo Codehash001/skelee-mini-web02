60e4874b0c8a068ae616a9b4e7f359a75139bb6e
Codehash001 <codehashblockchain@gmail.com>
2023-03-12 20:25:33 +0530

    update

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ function header() {
   return (
     <div className="flex flex-row justify-center items-center md:px-16 px-6 py-6 text-white">
       <div className="text-center flex flex-col md:items-start items-center font-Robodron w-full">
-        <h1 className="text-xl font-semibold uppercase md:text-start text-center">
+        <h1 className="text-3xl font-semibold uppercase text-center">
           galactaverse
         </h1>
       </div>
