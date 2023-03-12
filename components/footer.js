2e6df1a151c00c2a61d9dc876fe8704c1e5babba
Codehash001 <codehashblockchain@gmail.com>
2023-03-12 22:09:58 +0530

    update

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,13 @@
 export default function Footer(){
     return(
-        <div className=" bg-black w-screen h-auto md:px-16 px-4 py-4" id='footer'>
+        <div className=" bg-black w-screen h-auto md:px-16 px-4 py-6" id='footer'>
 
 <footer class="text-white font-Kanit">
 	<div className='flex items-center jutify-between w-full h-auto'>
 		<div className='flex flex-col items-start jutify-between w-full h-auto'>
 		  
 		    <img src='/galactaverse-logo.png' className='h-[9px] w-auto'/>
-		    <h1 className='text-[11px] mt-5'>&#169;2023 RARE STUDIOS NEW YORK.All Rights Reserved.</h1>
+		    <h1 className='text-[12px] mt-5'>&#169;2023 RARE STUDIOS NEW YORK.All Rights Reserved.</h1>
 		  
 		</div>
 		
