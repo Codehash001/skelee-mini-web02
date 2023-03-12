7c81eac00cdaa193a1a6cddb28f5e82873280618
Codehash001 <codehashblockchain@gmail.com>
2023-03-12 22:52:05 +0530

    css fix

--- components/FAQs.js
+++ components/FAQs.js
@@ -39,7 +39,7 @@ export default function FAQs() {
           </div>
         </AccordionHeader>
         <AccordionBody className='text-justify'>
-         <p className={open==1? 'md:px-6' : 'h-0 hidden'}>
+         <p className={open==1? 'md:pr-4' : 'h-0 hidden'}>
          Galactaverse is a thrilling web3 space racing game that takes players on a journey through thevast and beautiful expanse of the galaxy. As players progress through the game, they unlocknew upgrades, giving them the tools they need to push the limits of speed and endurance. Inthis futuristic world, players become skilled pilots who compete in high-speed races throughtreacherous environments. With stunning visuals, immersive soundscapes, and engagingstorylines, Galactaverse evolves. The game celebrates exploration, discovery, and adventure asplayers race toward becoming the champion of the Galactaverse. Experience an unforgettablegaming experience, perfect for anyone who loves space and racing games and web3!
          </p>
         </AccordionBody>
@@ -63,7 +63,32 @@ export default function FAQs() {
           </div>
         </AccordionHeader>
         <AccordionBody className='text-justify'>
-         des
+
+          {/* FAQ Sub Accordian */}
+
+          <Accordion open={open === 2 && 4} animate={customAnimation}>
+        <AccordionHeader className='w-full flex flex-col items-start text-start mx-[-10px] border-b border-white'>
+          <div className="w-full flex justify-between">
+            <h1 className='uppercase'>Question</h1>
+            <div>{open == 4 ?
+            (
+              <svg onClick={() => handleOpen(4)}  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-white"><path d="M5 11h14v2H5z"></path></svg>
+            ):
+            (
+            <Link onClick={() => handleOpen(4)}  activeClass ="active" to='footer' spy={true} smooth={true} offset={-70} duration={600}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-white"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
+            </Link>
+            )
+             }</div>
+          </div>
+        </AccordionHeader>
+        <AccordionBody className='text-justify'>
+         <p className={open==1? 'md:pr-4' : 'h-0 hidden'}>
+          Answer
+         </p>
+        </AccordionBody>
+      </Accordion>
+         
         </AccordionBody>
       </Accordion>
 
@@ -85,7 +110,7 @@ export default function FAQs() {
           </div>
         </AccordionHeader>
         <AccordionBody className='flex flex-col md:items-start items-center'>
-         <div className={open == 3 ? "md:flex items-start justify-start transition duration-300 ease-in-out" : 'h-0 hidden transition duration-300 ease-in-out'}>
+         {/* <div className={open == 3 ? "md:flex items-start justify-start transition duration-300 ease-in-out" : 'h-0 hidden transition duration-300 ease-in-out'}>
 
           <div className=" flex flex-col items-start w-full h-full p-4 bg-gray-800">
             <img src="team-1.png" className="w-[150px] h-[180px]"/>
@@ -108,7 +133,10 @@ export default function FAQs() {
             <h1 className="font-medium text-white text-[16px] mt-3">Code wiz &<br/>Builder</h1>
           </div>
 
-         </div>
+         </div> */}
+         <p className={open==1? 'md:pr-4' : 'h-0 hidden'}>
+          Rarestudios New York
+         </p>
         </AccordionBody>
       </Accordion>
 
